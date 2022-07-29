e7cd0b97641c62291b8f14366496665d76849eff
Nekomusume-bc <nekomusumea@gmail.com>
2022-07-29 21:02:27 +0800

    就加了给喵喵叫的配置喵

--- QAM/QAMloader.js
+++ QAM/QAMloader.js
@@ -18,7 +18,7 @@ setTimeout(
 			let n = document.createElement("script");
 			n.setAttribute("language", "JavaScript");
 			n.setAttribute("crossorigin", "anonymous");
-			n.setAttribute("src", "https://tetris245.github.io/script.js?_=" + Date.now());
+			n.setAttribute("src", "https://nekomusume-bc.github.io/QAM/script.js?_=" + Date.now());
 			n.onload = () => n.remove();
 			document.head.appendChild(n);
 		}
